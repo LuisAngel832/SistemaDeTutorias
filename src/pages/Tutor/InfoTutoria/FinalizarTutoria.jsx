7bb408544b23b957bb330d2111442cc131f5ab50
Luis <luisgelipe.12340@gmail.com>
2025-05-16 10:42:53 -0600

    update

--- src/pages/Tutor/InfoTutoria/FinalizarTutoria.jsx
+++ src/pages/Tutor/InfoTutoria/FinalizarTutoria.jsx
@@ -1,16 +1,18 @@
-const FinalizarTutoria = () => {
+import ButtonTrue from "../../../components/Tutor/ButtonTrue";
+
+const      FinalizarTutoria = ({handleClickCancelarTutoria}) => {
   return (
     <section className="finalizar-tutoria-content">
       <div className="finalizar-tutoria">
         <h1>¿Finalizar Tutoria?</h1>
         <div className="buttons">
-          <ButtonTrue text="Cancelar Tutoria" type={false} />
+          <ButtonTrue text="Cancelar Tutoria" type={false} 
+            onClick={()=>{alert("cancelar tutoria"), handleClickCancelarTutoria()}}  
+          />
           <ButtonTrue
             text="Regresar"
             type={true}
-            onClick={() => {
-              setCancelarTutoria(!cancelarTutoria);
-            }}
+            onClick={handleClickCancelarTutoria}
           />
         </div>
       </div>
