3d22ec4344884da1430e572edd6658e649a764f4
daniel <boregomendez2006@gmail.com>
2025-05-29 08:36:42 -0600

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import AppRouter from './Routes/AppRouter';
+import FormCrearTutoria from './pages/Tutor/CrearTutoria/formCrearTutoria';
+import CrearTutoria from './pages/Tutor/CrearTutoria/CrearTutoria';
+import LogIn from './pages/LogIn/LogIn';
+import Registro from './pages/Registro/Registro';
+import Home from './pages/Tutor/Home/Home';
 const App = () => {
   return (   
-    <AppRouter />
+    <Home/>
   );
 };
 
--- src/pages/Tutor/Home/Home.jsx
+++ src/pages/Tutor/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../../components/Tutor/Header";
 import TutoriasBox from "./Tutoriasbox";
 import "./home.css";    
+import "./homeR.css";
 const Home = () => {
     return (
         <div className="home">
